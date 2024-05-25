ee9241a32a4cecd120c74360cf6fcc8fa9fea283
Andy Ho <155758784+AndyHo04@users.noreply.github.com>
2024-05-25 19:16:35 -0400

    W10 Portfolio Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./w10/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header': '#2e3944',
+        'background':'#adbbda',
+        'link':'#5495ff',
+        'card':'#252222',
+
+      },
+    },
   },
   plugins: [],
 }
